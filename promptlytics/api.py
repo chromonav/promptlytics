0780e1c1b1cc578df2f98c0dcffe2b7816324907
Badal <badalsahani8381@gmail.com>
2023-04-24 12:03:39 +0000

    updated promp_log; added prompt publish and prompt get

--- promptlytics/api.py
+++ promptlytics/api.py
@@ -1,47 +1,64 @@
+import json
 import frappe
 
-# API mehtod: api/method/promptlytics.api.track
+# API method: api/method/promptlytics.api.track
 @frappe.whitelist()
 def track(**kwargs):
     try:
-        kwargs.pop('api_key')
-        kwargs.pop('cmd')
-        prompt = kwargs
-        response = kwargs.get('request_response')
         doc = frappe.new_doc('Prompt Log')
-        doc.prompt = prompt
-        doc.response = response
+        doc.function_name = kwargs.get("function_name")
+        doc.provider_type = kwargs.get("provider_type")
+        model_kwargs = kwargs.get("kwargs")
+        doc.model = model_kwargs.get("model")
+        doc.request_timeout = model_kwargs.get("request_timeout")
+        doc.max_tokens = model_kwargs.get("max_tokens")
+        doc.stream = model_kwargs.get("stream")
+        doc.temperature = model_kwargs.get("temperature")
+        doc.group = kwargs.get("tags")
+        doc.prompt = json.dumps(kwargs, indent=4)
+        doc.response = json.dumps(kwargs.get('request_response'), indent=4)
         doc.insert()
         return "Success"
     except Exception as e:
         print(e)
         return "Error"
 
-# API mehtod: api/method/promptlytics.api.library_get_prompt_template
+# API method: api/method/promptlytics.api.library_get_prompt_template
 @frappe.whitelist() 
 def library_get_prompt_template(**kwargs):
-    pass
+    prompt_name = kwargs.get('prompt_name')
+    prompt = frappe.get_doc('Prompt', prompt_name)
+    return {"prompt_name":prompt_name, "prompt_template": prompt.content}
 
 
-# API mehtod: api/method/promptlytics.api.library_publish_prompt_template
+# API method: api/method/promptlytics.api.library_publish_prompt_template
 @frappe.whitelist() 
 def library_publish_prompt_template(**kwargs):
-    pass
+    try:
+        prompt_name = kwargs.get('prompt_name')
+        doc = frappe.new_doc('Prompt')
+        doc.title = prompt_name
+        doc.content = kwargs.get('prompt_template')
+        doc.insert()
+        return "Success"
+    except Exception as e:
+        print(e)
+        return "Error"
 
 
-# API mehtod: api/method/promptlytics.api.library_track_prompt
+# API method: api/method/promptlytics.api.library_track_prompt
 @frappe.whitelist() 
 def library_track_prompt(**kwargs):
     pass
 
 
-# API mehtod: api/method/promptlytics.api.library_track_metadata
+# API method: api/method/promptlytics.api.library_track_metadata
 @frappe.whitelist() 
 def library_track_metadata(**kwargs):
     pass
 
 
-# API mehtod: api/method/promptlytics.api.library_track_score
+# API method: api/method/promptlytics.api.library_track_score
 @frappe.whitelist() 
 def library_track_score(**kwargs):
     pass
